8064155839b8677deb375643ed97fcdaf8aef302
Wendubeauty <chinweduabia1@gmail.com>
2023-04-20 10:33:01 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
