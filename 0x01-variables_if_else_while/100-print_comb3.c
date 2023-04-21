9ade10e907c20d28769b5b6fb01fdc6d24413f68
Wendubeauty <chinweduabia1@gmail.com>
2023-04-21 06:15:55 -0700

    100 print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,18 @@ int main(void)
 {
 	int n;
 	int m;
-	for (n = 0; n < 9; n++) {
-		 for (m = n + 1; m <= 9; m++) {
-			 putchar('0' + n);
-			 putchar('0' + m);
-			 if (n != 8 || m != 9) { // Don't add "," after last combination
-				 putchar(',');
-				 putchar(' ');
-			 }
-		 }
+
+	for (n = 0; n < 9; n++)
+	{
+		for (m = n + 1; m <= 9; m++)
+		{
+			putchar('0' + n);
+			putchar('0' + m);
+			if (n != 8 || m != 9)
+				putchar(',');
+				putchar(' ');
+		}
 	}
 	putchar('\n');
-return (0);
+	return (0);
 }
