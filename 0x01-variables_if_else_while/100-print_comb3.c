d2799cbfc9bbb513aeb5e28fd174a92c10fadc90
Wendubeauty <chinweduabia1@gmail.com>
2023-04-21 06:00:37 -0700

    print comb 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	for (n = 0; n < 9; n++) {
+		 for (m = n + 1; m <= 9; m++) {
+			 putchar('0' + n);
+			 putchar('0' + m);
+			 if (n != 8 || m != 9) { // Don't add "," after last combination
+				 putchar(',');
+				 putchar(' ');
+			 }
+		 }
+	}
+	putchar('\n');
+return (0);
+}
