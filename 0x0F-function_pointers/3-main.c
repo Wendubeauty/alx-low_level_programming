bdda252dd1c2ff0d92a1df53531191b6ee66b3a1
Wendubeauty <chinweduabia1@gmail.com>
2023-05-24 16:10:02 -0700

    functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,28 +11,29 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    op_func = get_op_func(argv[2]);
-    if (op_func == NULL || (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%')))
-    {
-        printf("Error\n");
-        return (1);
-    }
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL ||
+			(num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%')))
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
 
