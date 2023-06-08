99cd84cb603abcfc28fade2201e3971bb33f5d9b
Wendubeauty <chinwenduabia1@gmail.com>
2023-06-07 19:08:15 -0700

    task 9

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,4 +13,18 @@ void free_listint2(listint_t **head)
 	free_listint(*head);
 	*head = NULL;
 }
+/**
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the list.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
 
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
