162dd0c02cf213be291fbbc45ab3b585550f68ae
Wendubeauty <chinwenduabia1@gmail.com>
2023-06-21 22:42:03 -0700

    task 3 modified

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,10 +7,13 @@
 #include <fcntl.h>
 #include <stdint.h>
 
+/* prototypes */
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int code, const char *message);
 int main(int argc, char **argv);
 int main(int argc, char *argv[]);
 
