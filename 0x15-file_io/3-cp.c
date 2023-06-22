a1ff6db08aa7fad9e85ada4672ad6fdc2bfa79b2
Wendubeauty <chinwenduabia1@gmail.com>
2023-06-21 22:49:27 -0700

    task 3 modified

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - This program copies the content of a file to another file.
  * @argc: Argument count
  * @argv: Argument vector
  *
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from fil %s\n", argv[1]), exit(98);
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 	}
 
 	if (r_bytes == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from fil %s\n", argv[1]), exit(98);
 
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
