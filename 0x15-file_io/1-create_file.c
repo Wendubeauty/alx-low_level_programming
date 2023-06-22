22e68bc620bb0c8b92623bb01c9fb2fe5d701659
Wendubeauty <chinwenduabia1@gmail.com>
2023-06-21 17:23:46 -0700

    task 1 modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,41 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <string.h>
+
 /**
- * create_file - Creates a file and writes text content to it
- * @filename: The name of the file to be created
- * @text_content: The text content to write to the file
+ * create_file - Creates a file with the specified content
+ * @filename: Name of the file to create
+ * @text_content: Content to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
-	mode_t permissions = S_IRUSR | S_IWUSR;
+	int file, bytesWritten, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
-	if (fd == -1)
+	if (text_content != NULL)
+	{
+		while (text_content[length])
+			length++;
+	}
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
+		bytesWritten = write(file, text_content, length);
+		if (bytesWritten == -1 || bytesWritten != length)
 		{
-			close(fd);
+			close(file);
 			return (-1);
 		}
 	}
 
-	close(fd);
+	close(file);
 	return (1);
 }
 
