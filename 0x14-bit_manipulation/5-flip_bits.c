cb99f5f8e8fceddb10163e8a52a008ab9abe64d6
Wendubeauty <chinwenduabia1@gmail.com>
2023-07-07 05:45:49 -0700

    task 5 done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits that need to be flipped to get from one number to another.
+ * flip_bits - Returns the number of bits that need to be flipped
+ * to get from one number to another.
  * @n: The first number.
  * @m: The second number.
  *
